d11587c0239029b246203c0222e902ee53e77897
nitin095 <nitinshokeen9@gmail.com>
2018-08-12 05:35:11 +0530

    updated countries component

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -13,6 +13,10 @@ export class CountriesComponent implements OnInit {
 
   public region: string;
   public countries: any;
+  public population: number;
+  public lowestPopulationCountry: any;
+  public highestPopulationCountry: any;
+  public languages: any;
 
   constructor(private _route: ActivatedRoute, public router: Router, public httpService: HttpService, private location: Location) { }
 
@@ -21,11 +25,34 @@ export class CountriesComponent implements OnInit {
     this.httpService.getRegion(this.region).subscribe(
       data => {
         this.countries = data;
+        this.getStats(this.countries);
       },
       error => {
         console.log(error.errorMessage)
       }
     )
   }
+
+  getStats = (countries:any) => {
+    this.population = 0;
+    let maxPopulation =  Math.max(...countries.map(o => o.population));
+    let minPopulation = Math.min(...countries.map(c => c.population));
+    let allLanguages = [];
+    for(let country of countries){
+      this.population += country.population;
+      for(let l of country.languages ){
+        allLanguages.push(l.name);
+      }
+      if(country.population == maxPopulation){
+        this.highestPopulationCountry = { name: country.name, population: maxPopulation}
+      }
+      if(country.population == minPopulation){
+        this.lowestPopulationCountry ={ name: country.name, population: minPopulation}
+      }
+    }
+    this.languages = new Set(allLanguages)
+    console.log(this.languages)
+  }
+  // end getStats
 
 }
