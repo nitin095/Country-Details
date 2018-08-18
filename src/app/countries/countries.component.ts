ce10ecccd2fe516ac5469bf54e76c7c70920f32f
nitin095 <nitinshokeen9@gmail.com>
2018-08-19 01:57:42 +0530

    added filters

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Location } from '@angular/common';
-import { HttpService} from '../http.service'
+import { HttpService } from '../http.service'
 
 @Component({
   selector: 'app-countries',
@@ -17,6 +17,7 @@ export class CountriesComponent implements OnInit {
   public lowestPopulationCountry: any;
   public highestPopulationCountry: any;
   public languages: any;
+  public selectedCurrency: string;
 
   constructor(private _route: ActivatedRoute, public router: Router, public httpService: HttpService, private location: Location) { }
 
@@ -33,26 +34,45 @@ export class CountriesComponent implements OnInit {
     )
   }
 
-  getStats = (countries:any) => {
+  getStats = (countries: any) => {
     this.population = 0;
-    let maxPopulation =  Math.max(...countries.map(o => o.population));
+    let maxPopulation = Math.max(...countries.map(o => o.population));
     let minPopulation = Math.min(...countries.map(c => c.population));
     let allLanguages = [];
-    for(let country of countries){
+    for (let country of countries) {
       this.population += country.population;
-      for(let l of country.languages ){
+      for (let l of country.languages) {
         allLanguages.push(l.name);
       }
-      if(country.population == maxPopulation){
-        this.highestPopulationCountry = { name: country.name, population: maxPopulation}
+      if (country.population == maxPopulation) {
+        this.highestPopulationCountry = { name: country.name, population: maxPopulation }
       }
-      if(country.population == minPopulation){
-        this.lowestPopulationCountry ={ name: country.name, population: minPopulation}
+      if (country.population == minPopulation) {
+        this.lowestPopulationCountry = { name: country.name, population: minPopulation }
       }
     }
     this.languages = new Set(allLanguages)
     console.log(this.languages)
   }
   // end getStats
+
+  sortBy = (field: string) => {
+    if (this.countries.length > 1) {
+      if (field.endsWith('1')) {
+        field = field.slice(0, -1);
+        this.countries.sort((a, b) => {
+          if (a[field] < b[field]) return 1;
+          else if (a[field] > b[field]) return -1;
+          else return 0;
+        });
+      } else {
+        this.countries.sort((a, b) => {
+          if (a[field] < b[field]) return -1;
+          else if (a[field] > b[field]) return 1;
+          else return 0;
+        });
+      }
+    }
+  }//end sortBy
 
 }
--- src/app/search.pipe.ts
+++ src/app/search.pipe.ts
@@ -5,13 +5,33 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SearchPipe implements PipeTransform {
 
-  transform(items: any, searchText: any): any {
-    if(!items) return [];
-    if(!searchText) return items; 
+  transform(items: any, searchText: any, type?: any): any {
+    if (!items) return [];
+    if (!searchText) return items;
+    console.log('pipe called')
+    console.log('text: ' + searchText)
+    console.log('type: ' + type)
     searchText = searchText.toLowerCase();
-    return items.filter( it => {
-      return it.name.toLowerCase().includes(searchText);
-    });
+    if (type == 'language') {
+      console.log('inside language part')
+      return items.filter(it => {
+        for (let language of it.languages)
+          if (language.name.toLowerCase().includes(searchText))
+            return true
+      });
+    } else if (type == 'currency') {
+      console.log('inside currency part')
+      return items.filter(it => {
+        for (let currency of it.currencies) {
+          if (currency.name && currency.name.toLowerCase().includes(searchText))
+            return true
+        }
+      });
+    } else {
+      return items.filter(it => {
+        return it.name.toLowerCase().includes(searchText);
+      });
+    }
   }
 
 }
