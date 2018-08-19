d23282c44cfa7cfc895ff50f740384e517e21240
nitin095 <nitinshokeen9@gmail.com>
2018-08-20 02:34:21 +0530

    updated home view

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { faGlobe, faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+
+  faGlobe = faGlobe;
+  faSearch = faSearch;
 
   constructor() { 
     console.log('home component constructor called')
