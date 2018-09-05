979b04e4d057d946908c14942bdce80b667f992b
nitin095 <nitinshokeen9@gmail.com>
2018-09-06 00:28:29 +0530

    added shared module

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BackBtnComponent } from './back-btn/back-btn.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [BackBtnComponent]
+})
+export class SharedModule { }
